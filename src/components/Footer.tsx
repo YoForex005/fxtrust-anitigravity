7bad60f1bfe45273af07fbbbb1549566b56a0321
Sreejita Deb <mitadeb3@gmail.com>
2026-03-09 15:49:22 +0530

    social media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Footer.module.css';
-
-const SocialIcon = ({ path }: { path: string }) => (
-    <svg className={styles.socialIcon} viewBox="0 0 24 24" fill="currentColor">
-        <path d={path} />
-    </svg>
-);
+import {
+    FaFacebookF,
+    FaLinkedinIn,
+    FaQuora,
+    FaXTwitter,
+    FaYoutube,
+    FaRedditAlien,
+    FaMedium,
+    FaPinterestP,
+    FaTumblr
+} from 'react-icons/fa6';
 
 export default function Footer() {
     return (
@@ -20,8 +25,15 @@ export default function Footer() {
                             Building the platform we wish existed when we started. Less BS, more actual features.
                         </p>
                         <div className={styles.socialLinks}>
-                            <a href="#" aria-label="LinkedIn"><SocialIcon path="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" /></a>
-                            <a href="#" aria-label="Twitter"><SocialIcon path="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" /></a>
+                            <a href="https://www.facebook.com/fxtrusts" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><FaFacebookF className={styles.socialIcon} /></a>
+                            <a href="https://www.linkedin.com/company/fxtrusts/?viewAsMember=true" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FaLinkedinIn className={styles.socialIcon} /></a>
+                            <a href="https://www.quora.com/profile/FxTrust-Premium" target="_blank" rel="noopener noreferrer" aria-label="Quora"><FaQuora className={styles.socialIcon} /></a>
+                            <a href="https://x.com/FxTrusts" target="_blank" rel="noopener noreferrer" aria-label="X"><FaXTwitter className={styles.socialIcon} /></a>
+                            <a href="https://www.youtube.com/@FxTrusts" target="_blank" rel="noopener noreferrer" aria-label="YouTube"><FaYoutube className={styles.socialIcon} /></a>
+                            <a href="https://www.reddit.com/user/fxtrusts/" target="_blank" rel="noopener noreferrer" aria-label="Reddit"><FaRedditAlien className={styles.socialIcon} /></a>
+                            <a href="https://medium.com/@fxtrustspremium" target="_blank" rel="noopener noreferrer" aria-label="Medium"><FaMedium className={styles.socialIcon} /></a>
+                            <a href="https://in.pinterest.com/fxtrusts/" target="_blank" rel="noopener noreferrer" aria-label="Pinterest"><FaPinterestP className={styles.socialIcon} /></a>
+                            <a href="https://www.tumblr.com/blog/fxtrusts" target="_blank" rel="noopener noreferrer" aria-label="Tumblr"><FaTumblr className={styles.socialIcon} /></a>
                         </div>
                         <div className={styles.newsletter}>
                             <h4>Join 2,847 Brokers Getting Our Brutally Honest Emails</h4>
